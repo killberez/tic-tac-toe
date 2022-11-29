a5a3383458c5b02d638c89f2a510f207de7adc42
taras_liush <taras.liush@gmail.com>
2022-11-29 19:53:36 +0300

    tic-tac-toe front 2nd commit

---
+++ app/components/board.js
@@ -0,0 +1,50 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import fetch from 'fetch';
+
+
+async function getData() {
+    const data = await fetch('http://localhost:3000/api/motos').then(function (response) {
+        return response.json();
+    });
+}
+
+function calculateWinner(cells) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+            return cells[a]
+        }
+    }
+    return null
+}
+
+export default class BoardComponent extends Component {
+    @tracked board = Array(9).fill(null)
+    @tracked xIsNext = true
+    @tracked winner = calculateWinner(this.board)
+
+    @action
+    playerClick(index) {
+        const boardCopy = [...this.board]
+        if (this.winner || boardCopy[index]) return null
+        boardCopy[index] = this.xIsNext ? 'X' : '0'
+        this.board = boardCopy
+        this.xIsNext = !this.xIsNext
+        if (calculateWinner(this.board) !== null) {
+            console.log(`Player ${boardCopy[index]} won!`)
+        }
+        getData()
+    }
+}
